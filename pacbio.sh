17126947fa1a59cd39debc584643b603e9061e47
Amina Echchiki <amina.echchiki@unil.ch>
2016-10-04 11:33:49 +0200

    data and conversion

--- pacbio.sh
+++ pacbio.sh
@@ -1,4 +1,17 @@
 # 0) original data
+# from portal: http://smrt-lgtf.vital-it.ch/user/listanalysis
+
+# note: tmp done with subreads (fastq/a from bax.h5)
+# note2: where did I get ROI from? not on smrt.vital-it.ch (?)
+
+# original directory
+pacbio_roi=/scratch/beegfs/monthly/aechchik/PacBioData/ROI/
+# note: 3 fractions! 
+
+# convert to fasta
+cat $pacbio_roi/ROI_1-2k.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > $pacbio_roi/ROI_1-2k.fasta
+cat $pacbio_roi/ROI_2-3k.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > $pacbio_roi/ROI_2-3k.fasta
+cat $pacbio_roi/ROI_3-7k.fastq | awk '{if(NR%4==1) {printf(">%s\n",substr($0,2));} else if(NR%4==2) print;}' > $pacbio_roi/ROI_3-7k.fasta
 
 # 1) alignment
 
